86fbda266870329cb33945e753e54e80c28e9bc6
Parviz Alasgarov <alas.parviz@gmail.com>
2019-08-10 14:42:16 -0500

    added feature file

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,4 +1,21 @@
 package com.vytrack.runners;
 
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},// to specify where are the features
+        //features contains scenarios
+        //every scenario is like a test
+        glue = {"com/vytrack/step_definitions/LoginStepDefinitions.java"},//where is the implementation for features
+
+        //dryRun to generate step  definitions automatically
+        // u will see them in the console output
+        dryRun = true
+
+)
 public class CukesRunner {
 }
